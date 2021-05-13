4da4cb3a314746cc45fa0de6b934e03d60ce786f
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-13 19:04:25 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 02 - EXCEPTION/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCISE_02___EXCEPTION.Entities.Exceptions{
+    //------------------------------- START -------------------------------//
+    class DomainException : ApplicationException {
+        public DomainException(string message) : base(message) {
+        }
+    }
+    //-------------------------------- END -------------------------------//
+}
---
+++ EXERCISE 02 - EXCEPTION/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using EXERCISE_02___EXCEPTION.Entities;
+using EXERCISE_02___EXCEPTION.Entities.Exceptions;
+namespace EXERCISE_02___EXCEPTION {
+    //-----> START
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Holder: ");
+            String holder = Console.ReadLine();
+            Console.WriteLine("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine());
+            Console.WriteLine("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine());
+
+            Account acc = new Account(number, holder, balance, withdrawLimit);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine());
+            try {
+                acc.Withdraw(amount);
+                Console.WriteLine("New balance: " + acc.Balance.ToString("F2"));
+            }
+            catch (DomainException e) {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> END
+}
